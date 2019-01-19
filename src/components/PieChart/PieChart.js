c022cd2efd4c4e7bee621c63ada8d9eff4b70cef
Lingsi Ke -X (lingke - INFOSYS LIMITED at Cisco) <lingke@cisco.com>
2019-01-19 13:41:32 -0800

    'first'

---
+++ src/components/PieChart/PieChart.js
@@ -0,0 +1,98 @@
+import React from 'react';
+import axios from 'axios';
+import { Bar } from 'react-chartjs-2';
+import Chart from 'chart.js';
+
+class PieChart extends React.Component {
+
+    state = {
+        dataStore: {
+            labels: ['Boston', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
+            datasets: [
+                {
+                    label: 'Population',
+                    data:[
+                        617594,
+                        181045,
+                        153060,
+                        106519,
+                        105162,
+                        95072
+                    ],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.6)',
+                        'rgba(54, 162, 235, 0.6)',
+                        'rgba(255, 206, 86, 0.6)',
+                        'rgba(75, 192, 192, 0.6)',
+                        'rgba(153, 102, 255, 0.6)',
+                        'rgba(255, 159, 64, 0.6)',
+                        'rgba(255, 99, 132, 0.6)'
+                    ]
+                }
+            ]
+        },
+        dataChart: []
+    }
+
+    componentDidMount() {
+        axios.get("http://api.population.io:80/1.0/population/1980/aged/18/")
+        .then(res => {
+            console.log(res);
+            const populationDatas = res.data;
+            let showM = [];
+            const updatedData = populationDatas.map(element => {
+                let maleData = element.males;
+                let femaleData = element.females;
+                let countries = element.country;
+                showM.push(maleData);
+           
+                
+            });
+            // console.log(showM);
+
+            let dataChart = this.state.dataChart;
+            dataChart = new Chart('canvas', {
+                type: 'pie',
+                data: {
+                    datasets: [
+                        {
+                            data: showM,
+                            backgroundColor: 'rgba(255, 99, 132, 0.6)',
+                        }
+                    ]
+                },
+                options: {
+                    legend: {
+                        display: false
+                    }
+                }
+                
+            })
+            
+            console.log(dataChart);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+        
+    }
+
+    render() {
+        return (
+            <div className='chart'>
+                <Bar 
+                 data={this.state.dataStore}
+                 options={{}}
+                />
+                <canvas
+                    style={{ width: 800, height: 300 }}
+                    id="canvas"
+                />
+            </div>
+        )
+    }
+}
+
+
+export default PieChart;
